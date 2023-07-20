960b801016cf75bbc9c337f60dcdc362bd5c7e3c
Kevin Anderson <ksacamera@gmail.com>
2023-07-20 11:37:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class Car {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  getDescription() {
+    return `Make:${this.make}  Model:${this.model}  Year:${this.year}`;
+  }
+}
+
+class ElectricCar extends Car {
+  constructor(make, model, year, range) {
+      super(make, model, year);
+      this.range = range;
+    }
+
+    getDescription() {
+      return `${super.getDescription()}  Range:${this.range} miles`;
+    }
+}
+
+const car = new ElectricCar(`Tesla`, `Model S`, 2019, 300)
+console.log(car.getDescription());
